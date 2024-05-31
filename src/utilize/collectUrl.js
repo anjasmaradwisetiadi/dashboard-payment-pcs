613c7b5a3be060328b02f0832c32b1a6d30db5a9
Anjasmara Dwi Setiadi <enzoblackred@gmail.com>
2024-06-01 01:56:35 +0700

    [deployment] deployment 1/6/2023

--- src/utilize/collectUrl.js
+++ src/utilize/collectUrl.js
@@ -1,4 +1,4 @@
 export const collectUrl= {
-    baseUrl: 'http://localhost:5173/',
-    // baseUrl: ''
+    // baseUrl: 'http://localhost:5173/',
+    baseUrl: 'https://landing-page-54d28.web.app/'
 }
