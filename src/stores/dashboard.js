da1ac8524814ea15f6482a22a3068ff14fb00b9c
Anjasmara Dwi Setiadi <enzoblackred@gmail.com>
2024-05-31 16:30:18 +0700

    [master] successfull implement integration online user

--- src/stores/dashboard.js
+++ src/stores/dashboard.js
@@ -16,6 +16,10 @@ export const useDashboardStore = defineStore('dashboard', {
   },
 
   getters: {
-
+    getterPersonOnline(state) {
+      // handle show when online user just 10 user when data more than 10, reduce rendering element not used
+      const data = state.personOnline.data?.length >= 10 ? state.personOnline.data.slice(0,11): state.personOnline.data
+      return data
+    },
   }
 })
--- src/views/dashboard/UserStatus.vue
+++ src/views/dashboard/UserStatus.vue
@@ -8,23 +8,24 @@
             </div>
             <div class="relative flex-grow flex flex-col shadow-lg rounded-lg py-2 px-2 border mt-3">
                 <!-- ********** it show when online user more than 10 user-->
-                <!-- <div 
+                <div 
+                    v-if="getPersonOnline?.length >= 10"
                     class="relative flex justify-center z-10 min-h-20 pl-4">
                     <div
-                        v-for="(data, index) in dataDummyLength"
+                        v-for="(data, index) in getPersonOnline"
                         :key="index" 
                         class="absolute flex flex-col leading-4 text-center"
                         :class="[`z-${index+1}`, (index+1) === 1 ? 'left-0':`left-[calc(${index}*2rem)]` ]"
                     >   
                         <template v-if="(index+1) <= 9">
                             <div class="">
-                                <img class="w-10 h-10 rounded-full border-2 border-white" src="../../assets/image/user_1.png" alt="user_profile">
+                                <img class="w-10 h-10 rounded-full border-2 border-white" :src="collectUrl.baseUrl + data.image" alt="user_profile">
                             </div>
-                            <div class="font-semibold text-xs">
-                                Jahil{{ data }}
+                            <div class="text-[0.6rem] font-semibold leading-3">
+                                {{ data.name }}
                             </div>
-                            <div class="text-xs font-extralight">
-                                Sahid
+                            <div class="text-[0.6rem] font-extralight leading-3">
+                                {{ data.status_work }}
                             </div>
                         </template> 
                         <template v-if="(index+1) === 10">
@@ -33,28 +34,32 @@
                             </div>
                         </template>
                     </div>
-                </div> -->
+                </div>
                 
                 <!-- ********** it show when online user less than 9 user-->
-                <!-- <div class="relative flex justify-center z-10 min-h-20">
+                <div
+                    v-if="getPersonOnline?.length < 10" 
+                    class="relative flex justify-center z-10 min-h-20">
                     <div
-                        v-for="(data, index) in dataDummyLength" 
+                        v-for="(data, index) in getPersonOnline" 
                         :key="index"
                         class="relative flex flex-col leading-4 text-center ">
                         <div class="">
-                            <img class="w-14 h-12 rounded-full border-2 border-white bg-center bg-cover" src="../../assets/image/user_1.png" alt="user_profile">
+                            <img class="w-12 h-12 rounded-full border-2 border-white bg-center bg-cover" :src="collectUrl.baseUrl + data.image" alt="user_profile">
                         </div>
                         <div class="text-xs font-semibold">
-                            Jahil {{ index+1 }}
+                            {{ data.name }}
                         </div>
                         <div class="text-xs font-extralight">
-                            Sahid
+                            {{ data.status_work }}
                         </div>
                     </div>
-                </div> -->
+                </div>
 
                 <!-- ********** it show when no online -->
-                <div class="relative flex justify-center items-center text-center z-10 min-h-20">
+                <div
+                    v-if="!getPersonOnline?.length"
+                    class="relative flex justify-center items-center text-center z-10 min-h-20">
                     <div class="text-center font-semibold">
                         Not User Online
                     </div>
@@ -69,8 +74,11 @@ import { ref, reactive, watch, computed, onMounted, onBeforeMount } from 'vue';
 import {useDashboardStore} from '../../stores/dashboard';
 import {utilize} from '../../utilize/index'
 import {collectUrl} from '../../utilize/collectUrl'
+const dashboardStore = useDashboardStore()
 
-let dataDummyLength = 9
+const getPersonOnline = computed(()=>{
+    return dashboardStore.getterPersonOnline
+})
 </script>
 
 <style scoped>
